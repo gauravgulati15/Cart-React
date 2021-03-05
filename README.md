d5cf2b7640a9a79ac60a8b97f690141274f84f2c
Gaurav Gulati <56128284+gauravgulati15@users.noreply.github.com>
2021-03-05 19:20:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## React Project 8
+
+<hr>
+
+#### _**CART**_
+
+> _CONTEXT API & useReducer_ is used in this project.
+
+To view in your local system, fork the project and install the dependencies using the below commands:
+
+```shell
+>> npm install
+>> npm start
+```
+
+<br>
+<p align="center">
+  <img src="readmeimg\Hnet-image.gif" width="1000" title="cart">
+</p>
---
+++ src/reducer.js
@@ -0,0 +1,67 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  } else if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  } else if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  } else if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  } else if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  } else if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, loading: false, cart: action.payload };
+  }
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          } else {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error("no matching action type");
+};
+
+export default reducer;
